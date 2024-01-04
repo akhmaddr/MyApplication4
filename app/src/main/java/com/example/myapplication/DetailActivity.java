2ecc5b96a836b22b6eb521729a94ca16481c393b
akhmaddr <155619199+akhmaddr@users.noreply.github.com>
2024-01-04 16:54:37 +0700

    Update DetailActivity.java

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -56,6 +56,7 @@ public class   DetailActivity extends AppCompatActivity {
                 });
             }
         });
+        // tester
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
